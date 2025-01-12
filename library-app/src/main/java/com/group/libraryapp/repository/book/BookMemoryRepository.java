5196d0353a8fa26894c538cbeee6ee9ca347af02
dkdk-kang <dkdk.kang@samsung.com>
2025-01-12 17:32:47 +0900

    1st commit

---
+++ library-app/src/main/java/com/group/libraryapp/repository/book/BookMemoryRepository.java
@@ -0,0 +1,15 @@
+package com.group.libraryapp.repository.book;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+
+@Repository
+public class BookMemoryRepository implements BookRepository{
+
+    @Override
+    public void saveBook() {
+System.out.println("MemoryRepository");
+    }
+}
